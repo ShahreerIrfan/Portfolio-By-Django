59fedddfc447836093b38aed664682f46842ee7b
Your Name <mdshahreerirfan@gmail.com>
2024-02-10 16:47:20 +0600

    fully completd

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-31!i0+%i%2@%20brm9p7cmg=sdroc%=st@wb)#(z5u*qg&=46s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
